c619919fe369a6434ef17c835e71b1569d336120
tuckpuck <tucker.triggs@yahoo.com>
2019-03-14 17:38:20 -0600

    enqueue javascript scripts in functions.php

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
-$( ".nav-toggle" ).click(function() {
-  $(this).toggleClass("open");
-  $("nav").fadeToggle(100);
+jQuery(document).ready(function($) {
+  $(document).foundation();
+  $(".nav-toggle").click(function () {
+    $(this).toggleClass("open");
+    $("nav").fadeToggle(100);
 
-  return false;
+    return false;
+  });
 });
+
+
