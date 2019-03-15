5c6302ea84ad3691790111d173b797b3e4699d80
tuckpuck <tucker.triggs@yahoo.com>
2019-03-15 09:55:43 -0600

    removed static content and added dynamic php content to header.php and footer.php

--- header.php
+++ header.php
@@ -3,18 +3,16 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Treehouse WordPress Theme</title>
-    <link rel="stylesheet" href="assets/css/foundation.css" />
-    <link rel="stylesheet" href="assets/css/styles.css" />
-    <script src="assets/js/vendor/modernizr.js"></script>
+    <title><?php wp_title(); ?></title>
+    <?php wp_head(); ?>
   </head>
 
   <body>
     <header class="row no-max pad main">
-  <h1><a class='current' href="index.html">G</a></h1>
+  <h1><a class='current' href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
   <a href="" class="nav-toggle"><span></span>Menu</a>
   <nav>
-    <h1 class="open"><a class='current' href="index.html">G</a></h1>
+    <h1 class="open"><a class='current' href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
     <ul class="no-bullet">
       <li class="current parent"><a class='current' href="index.html">Portfolio</a>
         <ul class="sub-menu">
